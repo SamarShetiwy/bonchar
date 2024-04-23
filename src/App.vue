ef10ab90b25f47d2064015db7b2d14c09d7e37cd
samarshetiwy <samarshetiwy4791@gmail.com>
2024-04-24 00:08:40 +0200

    BOUNCER

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import Navigation from './components/Navigation.vue';
+
+</script>
+
+<template>
+ 
+<!-- <div class="container"> -->
+
+  <Navigation />
+  <RouterView />
+<!-- </div> -->
+</template>
+
+<style lang="scss">
+
+@import './assets/style.scss';
+
+</style>
+
---
+++ src/components/Navigation.vue
@@ -0,0 +1,44 @@
+<template >
+
+    <div class="">
+
+    <div class="flex justify-between w-100 ">
+        <div class=" flex m-3 pl-52 gap-4" >
+            <p>EN</p>
+            <p>USD</p>
+        </div>
+        <div class=" flex m-3 pr-52 ">
+            <i></i>
+            <p>My Profile</p>
+            <i></i>
+            <p>0 items $00 </p>
+            <i></i>
+
+
+        </div>
+    </div>
+
+    <div>
+        <h1 class="  uppercase text-red flex justify-center "> bouncer</h1>
+    <nav >
+      <ul class=" flex items-center justify-center gap-10 ">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">STORE</a></li>
+        <li><a href="#">IPHONE</a></li>
+        <li><a href="#">IPAD</a></li>
+        <li><a href="#">MACBOOK</a></li>
+        <li><a href="#">ACCESSORS</a></li>
+      </ul>
+    </nav>
+</div>
+</div>
+  </template>
+  
+  <script>
+ 
+  </script>
+  
+  <style lang="scss">
+  
+  </style>
+  
